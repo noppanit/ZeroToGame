50d0bc26b24c58a0f05603a700fd27853b74551f
Noppanit Charassinvichai <noppanit.c@gmail.com>
2011-11-05 11:14:53 +0000

    Player object and hacked to get it to work

--- zero2game.js
+++ zero2game.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
 	var canvas = $("#main-canvas");
 	var ctx = canvas[0].getContext("2d");
-	var img = new Image();
-	img.onload = function() {
-		ctx.drawImage(img, 0, 0);
-	};
-	img.src = "mozilla.jpeg";
+	
+	var player = new Player("mozilla.jpeg", 0, 0, ctx);
 	
 	var output = $("#output");
+	$(document).bind("keydown", function(event) {
+		output.html(event.which);
+	});
 
-	output.html("Hi, I'm Toy and Cesar. I'm learning ");
+	output.html("Hi, I'm Toy and Cesar. I'm learning");
   	
 });
+
+function Player(imgFile, x, y, ctx) {
+	var that = this;
+	this.x = x;
+	this.y = y;
+	this.ctx = ctx;
+	this.imageLoaded = false;
+	this.img = new Image();
+	this.img.onload = function() {
+		that.imageLoaded = true; 
+		that.draw(ctx);
+	};
+	this.img.src = imgFile;
+};
+
+Player.prototype = {
+	draw : function(ctx) {
+		if(this.imageLoaded) {
+			ctx.drawImage(this.img, this.x, this.y);
+		}
+	},
+	
+	move : function(dx, dy, ctx) {
+		this.x += dx;
+		this.y += dy;
+		this.draw(ctx);
+	}
+};
