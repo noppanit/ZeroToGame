8034c638cdff11123a3110d17ba0e59c07651b21
Noppanit Charassinvichai <noppanit.c@gmail.com>
2011-11-05 10:36:11 +0000

    Image loaded, change the image to wave

--- zero2game.js
+++ zero2game.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	var canvas = $("#main-canvas");
+	var ctx = canvas[0].getContext("2d");
+	var img = new Image();
+	img.onload = function() {
+		ctx.drawImage(img,0,0);
+	};
+	img.src = "wave.png";
+	
 	var output = $("#output");
 
 	output.html("Hi, I'm Toy and Cesar");
-  
+  	
 });
